addc03a228fb111ff7be60f3c434dd97feecf482
Abdul Wahab <abdul.wahab394.aw@gmail.com>
2021-08-24 01:45:49 +0500

    customer dialog added

--- src/components/customer/CustomerListToolbar.js
+++ src/components/customer/CustomerListToolbar.js
@@ -1,3 +1,5 @@
+import React, { useState } from 'react'
+
 import {
   Box,
   Button,
@@ -8,48 +10,52 @@ import {
   SvgIcon
 } from '@material-ui/core';
 import { Search as SearchIcon } from 'react-feather';
+import AddIcon from '@material-ui/icons/Add';
 
-const CustomerListToolbar = (props) => (
-  <Box {...props}>
-    <Box
-      sx={{
-        display: 'flex',
-        justifyContent: 'flex-end'
-      }}
-    >
-      {/* <Button>
-        Import
-      </Button>
-      <Button sx={{ mx: 1 }}>
-        Export
-      </Button> */}
-      <Button color="primary" variant="contained">
-        Add customer
-      </Button>
-    </Box>
-    <Box sx={{ mt: 3 }}>
-      <Card>
-        <CardContent>
-          <Box sx={{ maxWidth: 500 }}>
-            <TextField
-              fullWidth
-              InputProps={{
-                startAdornment: (
-                  <InputAdornment position="start">
-                    <SvgIcon fontSize="small" color="action">
-                      <SearchIcon />
-                    </SvgIcon>
-                  </InputAdornment>
-                )
-              }}
-              placeholder="Search customer"
-              variant="outlined"
-            />
-          </Box>
-        </CardContent>
-      </Card>
-    </Box>
-  </Box>
-);
+import Dialog from './../Dialog';
+import AddProducts from './../../components/product/AddProducts';
 
+const CustomerListToolbar = (props) => {
+  const [addCustomerOpen, setAddCustomerOpen] = useState(false)
+  return (
+    <Box {...props}>
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'flex-end'
+        }}
+      >
+        <Button onClick={() => setAddCustomerOpen(!addCustomerOpen)} color="primary" variant="contained" startIcon={<AddIcon />}>
+          Add customer
+        </Button>
+      </Box>
+      <Box sx={{ mt: 3 }}>
+        <Card>
+          <CardContent>
+            <Box sx={{ maxWidth: 500 }}>
+              <TextField
+                fullWidth
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <SvgIcon fontSize="small" color="action">
+                        <SearchIcon />
+                      </SvgIcon>
+                    </InputAdornment>
+                  )
+                }}
+                placeholder="Search customer"
+                variant="outlined"
+              />
+            </Box>
+          </CardContent>
+        </Card>
+      </Box>
+      {/* Customer Dialog */}
+      <Dialog open={addCustomerOpen} setOpen={setAddCustomerOpen}>
+        <AddProducts />
+      </Dialog>
+    </Box>
+  );
+}
 export default CustomerListToolbar;
--- src/components/product/AddProducts.js
+++ src/components/product/AddProducts.js
@@ -50,11 +50,13 @@ const AddProducts = () => {
         </div>
         <div className="col-lg-6 flex-column">
           <div style={{ position: 'relative' }}>
-            <div style={{ boxShadow: '0px 3px 6px rgba(0,0,0,0.16)', height: '263px', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+            <div style={{ boxShadow: '0px 3px 6px rgba(0,0,0,0.16)', height: '263px', display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column' }}>
               <img src={Image} style={{ width: '100px', height: '100px', objectFit: 'scale-down' }} />
+              <h5>google</h5>
             </div>
             <input type="file" style={{ position: 'absolute', top: '0px ', left: '0px', height: '100%', cursor: 'pointer', opacity: '0' }} />
           </div>
+
           <Button bgColor='#1774b0' text="Upload" />
         </div>
       </div>
